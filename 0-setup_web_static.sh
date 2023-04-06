117e0f47dbbca4cce89a238e54d4af60579d2f0b
OMmienu <nkedzi@gmail.com>
2023-04-06 15:00:51 -0700

    0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# An installation and configuration of Nginx
+
+#install nginx
+sudo apt-get update
+sudo apt-get install nginx -y
+
+#creating repositories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+#creating an html page(A Dummy One)
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+#creating a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#changing directory
+sudo chown -R ubuntu:ubuntu /data/
+
+#setting up the page to be served
+sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
+
+#restart the server
+sudo service nginx restart
